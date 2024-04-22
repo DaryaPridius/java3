7f834e38125851edac530cee9f364b4d0807a270
Pridius <darya.pridius@yandex.ru>
2024-04-22 19:40:50 +0300

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        int start = 100;
+        int x = 800;
+        int bonus;
+        if (x > 1000) {
+            bonus = x / 100;
+        } else {
+            bonus = 0;
+        }
+        int score = start + x;
+        System.out.println("Счёт:" + score);
+        System.out.println("Бонусные рубли:" + bonus);
+    }
 }
